121c596d4e83c4710347390a3cd2e3b4869c2b6c
Filip Tomaszewski <filip.tomaszewski@boombit.com>
2023-06-22 14:16:20 +0200

    Added asmdef

--- Assets/ContextMenu/Utils/PrefabProviderTool.cs
+++ Assets/ContextMenu/Utils/PrefabProviderTool.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
-using Z.ContextMenu;
 
-public class PrefabProxy
+namespace Z.ContextMenu
 {
-    // Use this for initialization
-    public IProvidePrefabs provider;
-    public Transform target { get; set; }
-    public MonoBehaviour source;
+	public class PrefabProxy
+	{
+		// Use this for initialization
+		public IProvidePrefabs provider;
+
+		public Transform target { get; set; }
+
+		public MonoBehaviour source;
+	}
 }
